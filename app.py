6b4b92933db30564619032f35ff2e58428195b7f
Adrianq88 <118317225+Adrianq88@users.noreply.github.com>
2024-09-19 10:16:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, render_template
-import pandas as pd
 from sneakersjoint import search_sku_on_site as search_sneakersjoint
-from aplug import scrape_aplug_product_by_sku as search_aplug
 
 app = Flask(__name__)
 
